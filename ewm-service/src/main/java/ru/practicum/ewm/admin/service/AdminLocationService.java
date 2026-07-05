6b4d2618295320ea1fcef7090b5c995b3ba4c742
Pthreea <lukas6289@outlook.com>
2026-07-05 11:11:46 +0400

    3.2.1

--- ewm-service/src/main/java/ru/practicum/ewm/admin/service/AdminLocationService.java
+++ ewm-service/src/main/java/ru/practicum/ewm/admin/service/AdminLocationService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface AdminLocationService {
     LocationDto createLocation(NewLocationDto dto);
+
     LocationDto updateLocation(Long locationId, NewLocationDto dto);
+
     void deleteLocation(Long locationId);
+
     LocationDto getLocationById(Long locationId);
+
     List<LocationDto> getAllLocations(int from, int size);
 }
